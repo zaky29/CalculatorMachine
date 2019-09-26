e967d45fdd5bb5187931cfacea4e01c1c15d3656
zaky dzakwan aziz <48462217+zaky29@users.noreply.github.com>
2019-09-26 12:12:43 +0700

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorMachine
+{
+    public class Calculator
+    {
+        public int Penambahan(int a, int b)
+        {
+            return a + b;
+        }
+        public int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+        public int Perkalian(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Pembagian(int a, int b)
+        {
+            return a / b;
+        }
+        public int Pangkat(int a, int b)
+        {
+            return (int)Math.Pow(a, b);
+        }
+        public static int Modulo(int a, int b)
+        {
+            return a % b;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CalculatorMachine;
+
+namespace CalculatorwindowsFormApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void btnHitung_Click(object sender, EventArgs e)
+        {
+            var a = int.Parse(txtNilaiA.Text);
+            var b = Convert.ToInt32(txtNilaiB.Text);
+
+            lstHasil.Items.Clear();
+            var cal = new Calculator();
+            lstHasil.Items.Add(string.Format("Hasil Penambahan: {0} + {1} = {2}", a, b, cal.Penambahan(a, b)));
+            lstHasil.Items.Add(string.Format("Hasil Pengurangan: {0} - {1} = {2}", a, b, cal.Pengurangan(a, b)));
+            lstHasil.Items.Add(string.Format("Hasil Perkalian: {0} * {1} = {2}", a, b, cal.Perkalian(a, b)));
+            lstHasil.Items.Add(string.Format("Hasil Pembagian: {0} / {1} = {2}", a, b, Calculator.Pembagian(a, b)));
+            lstHasil.Items.Add(string.Format("Hasil Pangkat: {0} ^ {1} = {2}", a, b, cal.Pangkat(a, b)));
+            lstHasil.Items.Add(string.Format("Hasil Modulo: {0} % {1} = {2}", a, b, Calculator.Modulo(a, b)));
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
++
